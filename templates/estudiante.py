515546d93f7e54dd4dcf8a192e2f66e4e1cc3d25
Pepu06 <pedrogonzalezsoro@gmail.com>
2025-06-04 18:58:58 -0300

    First commit

---
+++ templates/estudiante.py
@@ -0,0 +1,13 @@
+class Estudiante:
+    def __init__(self, ...):
+        ''' completar docstring '''
+        pass
+
+    def __repr__(self) -> str:
+        ''' completar docstring '''
+        pass
+
+    def __eq__(self, ...) -> ...:
+        ''' completar docstring '''
+        pass
+
