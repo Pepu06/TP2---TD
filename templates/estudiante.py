9427f4d700bdf9ac859759e6bf8082a2c3fc13b7
Pepu06 <pedrogonzalezsoro@gmail.com>
2025-06-05 17:51:49 -0300

    Inciso 2

--- templates/estudiante.py
+++ templates/estudiante.py
@@ -3,21 +3,21 @@ import csv
 class Estudiante:
     def __init__(self, provincia: str, puntaje_matematica: str, puntaje_lengua: str, puntaje_nse: str, ambito: str, sector: str):
         '''
-        Requiere: nada.
+        Requiere: ambito sea 'rural' o 'urbano'. sector sea 'estatal' o 'privado'.
         Devuelve: nada.
         Modifica: los atributos del objeto con los valores dados.
         '''
         self.provincia: str = provincia
-        self.puntaje_matematica: float = float(puntaje_matematica)
-        self.puntaje_lengua: float = float(puntaje_lengua)
-        self.puntaje_nse: float = float(puntaje_nse)
+        self.puntaje_matematica: float = puntaje_matematica
+        self.puntaje_lengua: float = puntaje_lengua
+        self.puntaje_nse: float = puntaje_nse
         self.ambito: str = ambito
         self.sector: str = sector
 
     def __repr__(self) -> str:
         '''
         Requiere: nada.
-        Devuelve: Un string con las caracteristicas del estudiante: su puntaje en matematica, seguido del puntaje de lengua, seguido de su puntaje de NSE (todos los puntajes con 2 decimales maximo), luego el ambito, sector y provincia del mismo.
+        Devuelve: Un string con las caracteristicas del estudiante: su puntaje en matematica, seguido del puntaje de lengua, seguido de su puntaje de NSE (todos los puntajes con 2 decimales maximo), luego el ambito de la escuela, el sector y provincia de la misma.
         Modifica: nada.
         '''
         return (f"<Mat:{self.puntaje_matematica:.2f}, " f"Len:{self.puntaje_lengua:.2f}, " f"NSE:{self.puntaje_nse:.2f}, " f"{self.ambito}, {self.sector}, {self.provincia}>")
@@ -42,9 +42,9 @@ with open('./datos_chico.csv') as f:
     for fila in reader:
         estudiante = Estudiante(
             provincia=fila['provincia'],
-            puntaje_matematica=fila['mpuntaje'],
-            puntaje_lengua=fila['lpuntaje'],
-            puntaje_nse=fila['NSE_puntaje'],
+            puntaje_matematica=float(fila['mpuntaje']),
+            puntaje_lengua=float(fila['lpuntaje']),
+            puntaje_nse=float(fila['NSE_puntaje']),
             ambito=fila['ambito'],
             sector=fila['sector']
         )
--- templates/resumen.py
+++ templates/resumen.py
@@ -1,13 +1,34 @@
 class Resumen:
-    def __init__(self, ...):
-        ''' completar docstring '''
-        pass
+    def __init__(self, cantidad: int, promedio_matematica: float, promedio_lengua: float, promedio_nse: float, proporcion_ambito_rural: float, proporcion_sector_estatal: float):
+        '''
+        Requiere: 0.0 <= proporcion_ambito_rural <= 1.0. 0.0 <= proporcion_sector_estatal <= 1.0
+        Devuelve: nada.
+        Modifica: los atributos del objeto con los valores dados.
+        '''
+        self.cantidad = cantidad
+        self.promedio_matematica = promedio_matematica
+        self.promedio_lengua = promedio_lengua
+        self.promedio_nse = promedio_nse
+        self.proporcion_ambito_rural = proporcion_ambito_rural
+        self.proporcion_sector_estatal = proporcion_sector_estatal
 
     def __repr__(self) -> str:
-        ''' completar docstring '''
-        pass
+        '''
+        Requiere: nada.
+        Devuelve: Un string con las caracteristicas del resumen: el promedio en matematica, seguido del promedio de lengua, seguido de promedio en NSE (todos los promedios con 2 decimales maximo), luego la proporción de estudiantes que van a una escuela del sector estatal, proporción de estudiantes que van a una escuela en el ámbito rural y la cantidad de estudiantes considerada en el resumen.
+        Modifica: nada.
+        '''
+        return (f"<Mat:{self.promedio_matematica:.2f}, " f"Len:{self.promedio_lengua:.2f}, " f"NSE:{self.promedio_nse:.2f}, " f"Rural:{self.proporcion_ambito_rural:.2f}, " f"Estado:{self.proporcion_sector_estatal:.2f}, " f"N:{self.cantidad}")
 
-    def __eq__(self, ...) -> ...:
-        ''' completar docstring '''
-        pass
+    def __eq__(self, r2: "Resumen") -> ...:
+        '''
+        Requiere: nada.
+        Devuelve: True si los dos resumenes son iguales, teniendo en cuenta una tolerancia de 0.001 en sus promedios y proporciones, False en caso contrario.
+        Modifica: nada.
+        '''
+        vr: bool = False
+        tolerancia: float = 0.001
+        if self.cantidad == r2.cantidad and abs(self.promedio_matematica - r2.promedio_matematica) < tolerancia and abs(self.promedio_lengua - r2.promedio_lengua) < tolerancia and abs(self.promedio_nse - r2.promedio_nse) < tolerancia and abs(self.proporcion_ambito_rural - r2.proporcion_ambito_rural) < tolerancia and abs(self.proporcion_sector_estatal - r2.proporcion_sector_estatal) < tolerancia:
+            vr = True
+        return vr
 
